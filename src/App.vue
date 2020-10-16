ef746ecf9e9e2be85dcdb0c6a5244498c3ee5991
Viotty Remi <remi.viotty@etudiant.univ-rennes1.fr>
2020-10-16 11:49:29 +0200

    git init

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Todos/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
+import Todos from "./components/todos.vue";
+
 
 @Component({
   components: {
-    HelloWorld,
+    Todos
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Material+Icons");
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { MdCard, MdButton, MdField, MdCheckbox } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
+
+Vue.use(MdCard);
+Vue.use(MdButton);
+Vue.use(MdField);
+Vue.use(MdCheckbox);
+
 
 new Vue({
   render: h => h(App),
